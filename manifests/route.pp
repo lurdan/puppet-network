77761de536565a82f5959b7b85c2def613524085
KURASHIKI Satoru <lurdan@gmail.com>
2012-08-03 14:42:04 +0900

    add newline to template.

--- manifests/route.pp
+++ manifests/route.pp
@@ -11,10 +11,10 @@ define network::route (
       concat::fragment {
         "network-route-${name}-up":
           target => '/etc/network/if-up.d/static-routes',
-          content => inline_template('route add -net <%= dest %><% if netmask %> netmask <%= netmask %><% end %> gw <%= gateway %> dev <%= device %>');
+          content => inline_template('route add -net <%= dest %><% if netmask %> netmask <%= netmask %><% end %> gw <%= gateway %> dev <%= device %>\n');
         "network-route-${name}-down":
           target => '/etc/network/if-down.d/static-routes',
-          content => inline_template('route del -net <%= dest %><% if netmask %> netmask <%= netmask %><% end %> gw <%= gateway %> dev <%= device %>');
+          content => inline_template('route del -net <%= dest %><% if netmask %> netmask <%= netmask %><% end %> gw <%= gateway %> dev <%= device %>\n');
       }
     }
     /(?i-mx:redhat|centos)/: {
